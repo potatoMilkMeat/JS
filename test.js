5a47cbed48795a44c6d12e703bd584dec51c9e0c
“chenglong_wavenet <549762433@qq.com>
2019-08-08 18:13:27 +0800

    Page5  2019-8-8 18:13

--- test.js
+++ test.js
@@ -1,4 +1,31 @@
-var a = '125px'
-// var num = 
-var aa = a.substring(0, (a.length - 2))
- console.log( aa)
+var  aa = function () {
+  console.log('aa')
+}
+var aaa =function() {
+  return {
+    a: aa,
+    b: function() {
+      console.log('b')
+    },
+    c: function() {
+      console.log('c')
+    }
+  }
+} 
+
+var a = aaa()
+var b = aaa()
+aa = function () {
+  console.log('aaa')
+}
+// a.b()
+// a.c()
+a.b = function() {
+  console.log('sssss')
+}
+
+a.a()
+b.a()
+
+a.b()
+b.b()
