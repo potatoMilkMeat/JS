d42080dd306eab0302d7e3b69fbec6cc8234c62a
网波-研发中心-程龙 <549762433@qq.com>
2020-11-24 17:59:00 +0800

    P15页

--- book/JavaScript语言精粹/main.js
+++ book/JavaScript语言精粹/main.js
@@ -1,4 +1,4 @@
-// 给函数类  定义链式新方法
+// P4 给函数类  定义链式新方法
 Function.prototype.method = function (name, func) {
   this.prototype[name] = func;
   return this;
@@ -15,3 +15,19 @@ A.method('log', function () {
 A.prototype.log(1, 2, 3);
 new A().log(123);
 
+// P10 switch while do for 前置label，可用break label; 来打断代码块
+var count=0;
+farther:
+for(var i=0;i<3;i++){
+  count++;
+  console.log('father循环前： ', count, i);
+  son: for(var j=0;j<5;j++){
+    // if(count >= 10){break;}
+    if(count >= 10){break farther;}
+    count++;
+    console.log('son循环： ', count, i, j);
+  }
+}
+
+// if(count >= 10){break;}          count 是 11 = (1+5) + (1+3) + (1)
+// if(count >= 10){break farther;}  count 是 10 = (1+5) + (1+3)
