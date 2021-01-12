e891c1a17427b50231b8f45453d5f58cbd1c162b
网波-研发中心-程龙 <549762433@qq.com>
2021-01-12 15:10:17 +0800

    serve 能访问到

---
+++ book/Vue-2源码解读/writeDom.js
@@ -0,0 +1,12 @@
+// 写入dom
+var writeDom = function(arr){
+  var cont = document.getElementById('cont'),
+      html = '';
+  for(var i=0,len = arr.length;i<len;i++){
+    item = arr[i];
+    html += '<p>'+ item.p + '</p>';
+    html += '<code>'+ item.code + '</code>';
+  }
+  cont.innerHTML = html;
+};
+// console.log('writeDom onReady!');
